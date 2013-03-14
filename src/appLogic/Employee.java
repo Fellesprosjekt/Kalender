18341c55c173d3569a113fc0afa72e8bf287cd61
Peter Sandberg <peter.sandberg@live.no>
2013-03-14 15:13:16 +0100

    Endringer i employee

--- src/appLogic/Employee.java
+++ src/appLogic/Employee.java
@@ -1,16 +1,29 @@
 package appLogic;
 
+import java.util.ArrayList;
+
 public class Employee extends User{
 
 	private String firstname;
 	private String lastname;
 	
 	
-	public Employee(String name) {
-		// TODO Auto-generated constructor stub
+	public Employee(String name, String email) throws InvalidNameException{
+		super(email);
+		setEmployeeName(name);
 	}
 
-	private void setEMployeeName(String name) {
+	private void setEmployeeName(String name) throws InvalidNameException {
+		if(!validateName(name)) throw new InvalidNameException();
+		else{
+			String[] fullname = name.split(" ");
+			firstname = fullname[0];
+			for(int i = 1; i<fullname.length; i++){
+				lastname+=fullname[i];
+				lastname+=" ";
+			}
+			lastname.trim();
+		}	
 	}
 	
 	public String getFirstNnme(){
@@ -19,5 +32,18 @@ public class Employee extends User{
 	
 	public String getLastname(){
 		return firstname;
+	}
+	
+	private boolean validateName(String name){
+		String[] fullname = name.split(" ");
+		if(fullname.length>=2){
+			for(String s : fullname){
+				s.trim();
+				for(int i = 0; i < s.length(); i++){
+					if(!Character.isLetter(name.charAt(i))) break;
+				}
+			}
+		}
+		return false;
 	}
 }
