071b975fd276f3e2083e2cac1f27f1288bf1e6a3
unknown <padalby@stud.ntnu.no>
2013-03-18 11:54:41 +0100

    createEmployee

--- src/gui/Home.java
+++ src/gui/Home.java
@@ -2,11 +2,34 @@ package gui;
 
 import java.util.Scanner;
 
+import exceptions.InvalidEmailException;
+import exceptions.InvalidNameException;
+
+import appLogic.Employee;
+
 public class Home {
 	Scanner sc;
 	
 	public Home(){
 		sc = new Scanner(System.in);
+	}
+	
+	private Employee createEmployee(){
+		System.out.println("Name: ");
+		String name = sc.nextLine();
+		System.out.println("Email: ");
+		String email = sc.nextLine();
+		System.out.println();
+		try {
+			return new Employee(name, email);
+		} catch (InvalidNameException e) {
+			System.err.println("Ugyldig navn");
+			createEmployee();
+		} catch (InvalidEmailException e) {
+			System.err.println("Ugyldig email");
+			createEmployee();
+		}
+		return null;
 	}
 	
 	private void createGroup(){
@@ -16,12 +39,9 @@ public class Home {
 		String name = sc.nextLine();
 	}
 	
-	private void createEmployee(){
-		
-	}
-	
 	public static void main(String[] args) {
 		Home h = new Home();
 		h.createGroup();
+		h.createEmployee();
 	}
 }
