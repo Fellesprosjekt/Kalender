e3db5243d3274baf1a0d8d3e081edb02fa517551
Peter Sandberg <peter.sandberg@live.no>
2013-03-18 11:54:24 +0100

    gjort ferdig creategroup

--- src/gui/Home.java
+++ src/gui/Home.java
@@ -1,19 +1,40 @@
 package gui;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import exceptions.InvalidEmailException;
+import exceptions.InvalidNameException;
+
+import appLogic.Employee;
+import appLogic.Group;
 
 public class Home {
 	Scanner sc;
+	ArrayList<Group> groups;
+	ArrayList<Employee> employees;
 	
 	public Home(){
 		sc = new Scanner(System.in);
 	}
 	
-	private void createGroup(){
-		System.out.print("Email: ");
-		String email = sc.nextLine();
+	private Group createGroup(){
 		System.out.print("Name: ");
 		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.println();
+		try{
+			Group g = new Group(name,email);
+			return g;
+		}catch(InvalidNameException e){
+			System.err.println("Ugyldig navn.");
+			createGroup();
+		}catch(InvalidEmailException e){
+			System.err.println("Ugyldig epost.");
+			createGroup();
+		}
+		return null;
 	}
 	
 	private void createEmployee(){
