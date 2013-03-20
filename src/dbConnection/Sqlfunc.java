9f2fb15993160508a4c32e442a9f4d2622a67c76
Anniken Hofgaard <annikenhofgaard@dhcp-016051.wlan.ntnu.no>
2013-03-20 10:56:06 +0100

    Lagt til manglende sqlfuncs

--- src/dbConnection/Sqlfunc.java
+++ src/dbConnection/Sqlfunc.java
@@ -3,6 +3,8 @@ package dbConnection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import org.joda.time.DateTime;
+
 import appLogic.Appointment;
 import appLogic.Employee;
 import appLogic.User;
@@ -37,7 +39,17 @@ public class Sqlfunc {
 	}
 	
 	
+	private ArrayList<Employee> loadEmployees(){
+		ArrayList<Employee> employees = new ArrayList<Employee>();
+		db.get("SELECT * FROM Calendar.CalendarUser WHERE UType = 'Employee'");
+		return null;
+	}
+	
 	private int getAppointmentId(Appointment a) {
+		return 0;
+	}
+	
+	private int getEmpId() {
 		return 0;
 	}
 	
@@ -46,15 +58,28 @@ public class Sqlfunc {
 		return 0;
 	}
 	
-	private void saveParticipant(Appointment a, User u){
+	public void saveParticipant(Appointment a, User u){
 		int aId = getAppointmentId(a);
 		int uId = getUserId(u);
 		db.send(String.format("INSERT INTO AppInvitation VALUES ('%s', '%s', null)",aId,uId));
 	}
 	
-	private ArrayList<Employee> loadEmployees(){
-		ArrayList<Employee> employees = new ArrayList<Employee>();
-		db.get("SELECT * FROM Calendar.CalendarUser WHERE UType = 'Employee'");
-		return null;
+	public void createAppiontment(Appointment a) {
+		DateTime start = a.getStart();
+		DateTime end = a.getEnd();
+		String desc = a.getDescription();
+		Employee leader = a.getLeader();
+		int lId=getEmpId();
+		db.send(String.format("INSERT INTO Appointment VALUES ('?', '%s', '%s','%s', '%s')",start, end, desc, lId));
+	}
+	
+	public void deleteParticipant(Appointment a, User u) {
+		int aId = getAppointmentId(a);
+		int uId = getUserId(u);
+		db.send(String.format("DELETE FROM AppInvitation WHERE AppID = '%s' AND UserID = '%s')", aId, uId));
+		}
+	
+	public void updateUserStatus (boolean status, Appointment a) {
+		
 	}
 }
