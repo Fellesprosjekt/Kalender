1ae315d6eb8f6f02c207f5d6fadfa7aa044ad60b
Peter Sandberg <peter.sandberg@live.no>
2013-03-20 09:48:48 +0100

    litt mer i init for testing, fikset litt på alarmpanel

--- src/appLogic/Room.java
+++ src/appLogic/Room.java
@@ -9,26 +9,19 @@ import exceptions.RoomBookedException;
 
 public class Room implements AppointmentListener{
 	public static ArrayList<Room> rooms = new ArrayList<Room>();
-	private String id;
+	private final String id;
 	private final int size;
 	private Calendar room_calendar;
 	
-	public Room(int size) {
+	public Room(String id, int size) {
 		this.size = size;
-		this.id = null;
+		this.id = id;
 		this.room_calendar = new Calendar();
 		rooms.add(this); 
 	}
 	
 	public String getId() {
 		return id;
-	}
-
-	public void setId(String id) {
-		if(this.id.equals(null)){
-			if(id.length()>4) this.id=id.substring(0,4);
-			else this.id=id;
-		}
 	}
 
 	public int getSize() {
