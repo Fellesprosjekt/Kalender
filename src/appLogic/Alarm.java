2f1b02d79686f9de99542d727a4c6231be271b0a
Steffen Birkeland <Steffenf.birkeland@gmail.com>
2013-03-20 15:52:03 +0100

    db add og remove alarm

--- src/appLogic/Alarm.java
+++ src/appLogic/Alarm.java
@@ -7,11 +7,17 @@ public class Alarm {
 	private final DateTime time;
 	private String label;
 	private final Appointment appointment;
+	private final int offset;
 	
-	public Alarm(DateTime time, String label, Appointment appointment) {
+	public Alarm(DateTime time, String label, Appointment appointment,int offset) {
 		this.time = time;
 		this.appointment=appointment;
+		this.offset=offset;
 		setLabel(label);
+	}
+	
+	public int getOffset(){
+		return this.offset;
 	}
 	
 	public String getLabel() {
