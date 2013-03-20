f390e3df5dda10d32fb79d28675675693bc25343
Anniken Hofgaard <annikenhofgaard@dhcp-016051.wlan.ntnu.no>
2013-03-20 13:01:59 +0100

    gjort noe i dbapp

--- src/dbConnection/DBAppointments.java
+++ src/dbConnection/DBAppointments.java
@@ -1,28 +1,24 @@
 package dbConnection;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.joda.time.DateTime;
 
 import appLogic.Appointment;
 import appLogic.Employee;
 import appLogic.User;
+import dbConnection.*;
+
 
 public class DBAppointments {
-	private int getAppointmentId(Appointment a) {
-		return 0;
-	}
 	
-	private int getEmpId() {
-		return 0;
-	}
+	Simpleconnect db = new Simpleconnect("test", null);
 	
-	
-	private int getUserId(User u) {
-		return 0;
-	}
 	
 	public void saveParticipant(Appointment a, User u){
-		int aId = getAppointmentId(a);
-		int uId = getUserId(u);
+		int aId = a.getId();
+		int uId = u.getId();
 		db.send(String.format("INSERT INTO AppInvitation VALUES ('%s', '%s', null)",aId,uId));
 	}
 	
@@ -30,41 +26,47 @@ public class DBAppointments {
 		DateTime start = a.getStart();
 		DateTime end = a.getEnd();
 		String desc = a.getDescription();
-		Employee leader = a.getLeader();
-		int lId=getEmpId();
+		int lId = a.getLeader().getId();
 		db.send(String.format("INSERT INTO Appointment VALUES ('?', '%s', '%s','%s', '%s')",start, end, desc, lId));
 	}
 	
 	public void deleteParticipant(Appointment a, User u) {
-		int aId = getAppointmentId(a);
-		int uId = getUserId(u);
-		db.send(String.format("DELETE FROM AppInvitation WHERE (AppID = '%s' AND UserID = '%s'))", aId, uId));
+		int aId = a.getId();
+		int uId = u.getId();
+		db.send(String.format("DELETE FROM AppInvitation WHERE (AppID = '%s' AND UserID = '%s')", aId, uId));
 		}
 	
 	public void updateUserStatus (boolean status, Appointment a, User u) {
-		int aId = getAppointmentId(a);
-		int uId = getUserId(u);
-		db.send(String.format("Update AppInvitation SET Confirmed = %s WHERE (AppID = '%s' AND UserID = '%s'))", status, aId, uId));
+		int aId = a.getId();
+		int uId = u.getId();
+		db.send(String.format("UPDATE AppInvitation SET Confirmed = %s WHERE (AppID = '%s' AND UserID = '%s')", status, aId, uId));
 	}
 	
 	public void updateAppDesc(Appointment a, String desc) {
-		int aId = getAppointmentId(a);
+		int aId = a.getId();
 		db.send(String.format("Update AppInvitation SET Description = %s WHERE AppID = '%s'", desc, aId));
 	}
 	
 	public void updateAppointmentStart(Appointment a, DateTime start) {
-		int aId = getAppointmentId(a);
+		int aId = a.getId();
 		db.send(String.format("Update AppInvitation SET startTime = %s WHERE AppID = '%s'", start, aId));
 	}
 	
 	public void updateAppointmentEnd(Appointment a, DateTime end) {
-		int aId = getAppointmentId(a);
+		int aId = a.getId();
 		db.send(String.format("Update AppInvitation SET endTime = %s WHERE AppID = '%s'", end, aId));
 	}
 	
-	public void removeAppointment(Appointment a) {
-		int aId = getAppointmentId(a);
-		db.send(String.format("DELETE FROM Appointment WHERE AppID = '%s')", aId);
+	public void deleteAppointment(Appointment a) {
+		int aId = a.getId();
+		db.send(String.format("DELETE FROM Appointment WHERE AppID = '%s'", aId));
 	}
+	
+	public Appointment getAppointment(int id) {
+		ArrayList<HashMap<String,String>>app=db.get(String.format("SELECT * FROM Appointment WHERE AppID = '%s'", id));
+		
+	}
+	
+	
 	
 }
