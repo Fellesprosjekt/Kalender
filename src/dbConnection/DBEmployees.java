c9dd5f43c400563deda98b51f3ee546e13e8a3b6
Peter Sandberg <peter.sandberg@live.no>
2013-03-20 13:16:14 +0100

    innlasting av ansatte og grupper

--- src/dbConnection/DBEmployees.java
+++ src/dbConnection/DBEmployees.java
@@ -1,5 +1,40 @@
 package dbConnection;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import exceptions.InvalidEmailException;
+import exceptions.InvalidNameException;
+
+import appLogic.Employee;
+
 public class DBEmployees {
+	Simpleconnect db;
 	
+	public DBEmployees(){
+		db = new Simpleconnect("Calendar", "");
+	}
+	
+	public ArrayList<Employee> loadEmployees(){
+		ArrayList<Employee> employees = new ArrayList<Employee>();
+		ArrayList<HashMap<String,String>> posts = db.get("SELECT * FROM Calendar.CalendarUser WHERE UType='Employee'");
+		for(HashMap<String,String> post : posts){
+			int id = Integer.parseInt(post.get("UserID"));
+			String email = post.get("Email");
+			String name = post.get("UName");
+			try {
+				Employee e = new Employee(id, name, email);
+				employees.add(e);
+			} catch (InvalidNameException e) {
+				deleteEmployee(id);
+			} catch (InvalidEmailException e) {
+				deleteEmployee(id);
+			}
+		}
+		return employees;
+	}
+	
+	public void deleteEmployee(int id){
+		db.send(String.format("DELETE FROM Calendar.CalendarUser WHERE UserID = %s", id));
+	}
 }
--- src/dbConnection/DBGroups.java
+++ src/dbConnection/DBGroups.java
@@ -1,5 +1,39 @@
 package dbConnection;
 
-public class DBGroups {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import appLogic.Group;
+import exceptions.InvalidEmailException;
+import exceptions.InvalidNameException;
+
+public class DBGroups {
+	Simpleconnect db;
+	
+	public DBGroups(){
+		db = new Simpleconnect("Calendar", "");
+	}
+	
+	public ArrayList<Group> loadGroups(){
+		ArrayList<Group> groups = new ArrayList<Group>();
+		ArrayList<HashMap<String,String>> posts = db.get("SELECT * FROM Calendar.CalendarUser WHERE UType='Group'");
+		for(HashMap<String,String> post : posts){
+			int id = Integer.parseInt(post.get("UserID"));
+			String email = post.get("Email");
+			String name = post.get("UName");
+			try {
+				Group g = new Group(id, name, email);
+				groups.add(g);
+			} catch (InvalidNameException e) {
+				deleteGroup(id);
+			} catch (InvalidEmailException e) {
+				deleteGroup(id);
+			}
+		}
+		return groups;
+	}
+	
+	public void deleteGroup(int id){
+		db.send(String.format("DELETE FROM Calendar.CalendarUser WHERE UserID = %s", id));
+	}
 }
